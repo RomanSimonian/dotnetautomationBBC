711a2f0aedbdf3dd0345c41b857c871d68d76a57
Oleksii Sokolov <laegolasse@gmail.com>
2022-01-19 20:07:24 +0200

    - lots of stuff

--- TestSolution/Class1.cs
+++ TestSolution/Class1.cs
@@ -1,14 +1,20 @@
-﻿namespace TestSolution
+﻿using NUnit.Framework;
+
+namespace TestSolution
 {
+    [TestFixture]
     public class Class1
     {
-        //blablabal whiskas
-        //blablabal whiskas
-        //blablabal whiskas
-        //blablabal whiskas
-        //blablabal whiskas
-        //blablabal whiskas
-        //blablabal whiskas
-        //blablabal whiskas
+        [TestCase("oleksii", 1)]
+        [TestCase("vladislav", 0)]
+        [TestCase("oleksii", 3)]
+        [TestCase("oleksii", 4)]
+        [TestCase("oleksii", 5)]
+        [TestCase("oleksii", 6)]
+        [Test]
+        public void Test1(string userName, int expectedUserRoleId)
+        {
+            Assert.Fail();
+        }
     }
 }
