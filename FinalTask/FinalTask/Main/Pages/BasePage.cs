4dd8c79ddd5553b85fbdcd34d28f1e667bc1007e
Bohdan Matiashchuk <muks.bodya@gmail.com>
2022-01-21 20:22:33 +0200

    Added BasePage

---
+++ FinalTask/FinalTask/Main/Pages/BasePage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+
+namespace FinalTask.Main.Pages
+{
+	public class BasePage
+	{
+		protected IWebDriver Driver;
+		public double defaultTime = 30;
+
+		public BasePage(IWebDriver driver)
+		{
+			this.Driver = driver;
+			PageFactory.InitElements(driver, this);
+		}
+
+		public void ImplicitlyWait(double defaultTime)
+		{
+			WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(defaultTime));
+		}
+		public IWebElement WaitAndReturmElementExist(double defaultTime, string xpathElement)
+		{
+			WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(defaultTime));
+			wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+			var element = wait.Until(x => x.FindElement(By.XPath(xpathElement)));
+			return element;
+		}
+
+		public void WaitUntilEnable(double defaultTime, IWebElement element)
+		{
+			WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(defaultTime));
+			wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+			wait.Until<bool>(x => element.Enabled);
+		}
+	}
+}
