9655682c68ef3126f1b40eec326654299e10ba5e
MykolaMaiboroda <mayba97@gmail.com>
2022-01-26 16:48:38 +0200

    Add task 1

---
+++ TASK_1_TEST/Pages/HomePage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK_1_TEST.Pages
+{
+    class HomePage
+    {
+    }
+}
---
+++ TASK_1_TEST/WaitUntil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+
+namespace TASK_1_TEST
+{
+    public static class Waiter
+    {
+        public static void ImplicitWait(IWebDriver webDriver, int time = 60) {
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(time);
+        }
+
+        public static void WaitElement(IWebDriver webDriver, By path, int time = 60) {
+            new WebDriverWait(webDriver, TimeSpan.FromSeconds(time)).Until(ExpectedConditions.ElementIsVisible(path));
+            new WebDriverWait(webDriver, TimeSpan.FromSeconds(time)).Until(ExpectedConditions.ElementToBeClickable(path));
+        }
+    }
+}
